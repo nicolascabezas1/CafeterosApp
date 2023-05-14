6bd2d97cb5508dd55deb7b50bc880a6f8f73c873
Nicolás Cabezas <cabezasn628@gmail.com>
2023-05-13 22:21:06 -0500

    feat(view login and button login): login view and screen navigation
    structure of login buttons and navigation from home to login view

--- app/src/main/java/com/cursoplatzi/cafeterosapp/MainActivity.kt
+++ app/src/main/java/com/cursoplatzi/cafeterosapp/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.cursoplatzi.cafeterosapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.cursoplatzi.cafeterosapp.login_view.LoginActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btCafeterosLogin: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initComponents()
+        initListeners()
+    }
+
+    private fun initComponents() {
+        btCafeterosLogin = findViewById(R.id.btCafeterosLogin)
+    }
+
+    private fun initListeners() {
+        btCafeterosLogin.setOnClickListener { navigateToLogin() }
+    }
+
+    private fun navigateToLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
